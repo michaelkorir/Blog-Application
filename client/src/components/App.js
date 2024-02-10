7543476c9351586a9e7d4f5bc0808fcff95e34b4
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-02-10 15:25:06 +0300

    Compile components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,19 +6,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Comments from './Comments';
 import ContactUs from './ContactUs';
 import AboutUs from './AboutUS';
-import HomePage from './Homepage';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Switch>
+        <Route path="/home" component={Home} />
         <Route path="/users" component={Users} />
-        <Route exact path="/blogs" component={Blogs} />
+        <Route path="/blogs" component={Blogs} />
         <Route path="/comments" component={Comments} />
         <Route path='/contacts' component={ContactUs}/>
         <Route path='/about' component={AboutUs} />
-        <Route path='/homepage' component={HomePage} />
       </Switch>
     </Router>
   );
