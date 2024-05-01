8026b95b0242a26a4e4901ca67dbdf710d5918a8
Michael Kiptoo <145563676+michaelkorir@users.noreply.github.com>
2024-05-01 23:08:40 +0300

    Update AboutUS.js

--- client/src/components/AboutUS.js
+++ client/src/components/AboutUS.js
@@ -6,7 +6,7 @@ const AboutUs = () => {
   return (
     <div className="about-us">
       <h4>About Us</h4>
-      <h5>Welcome to our blog!!!</h5>
+      <h5>Welcome to HiviPunde Blog!!!</h5>
       <p>We are passionate writers who love sharing our thoughts, ideas, and experiences with the world through this platform.</p>
       <p>Our mission is to provide valuable content on various topics, including technology, lifestyle, travel, and more.</p>
       <p>Feel free to explore our blog, engage with our content, and join us on our journey!</p>
